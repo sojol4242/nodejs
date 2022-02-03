2d179a44b486140a46f705c2b7289d791fc7e963
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-02-04 00:35:19 +0600

    learn module

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("hello global",global);
+
+const number=require('./number');
+console.log(number.number);
+// console.log(number.double([4,6,3,7]));
+
+// setTimeout(() => {
+//    let a=5;
+//    console.log(a);
+// }, 1000);
+
+// console.log(__dirname); 
+// console.log(__filename);
+
